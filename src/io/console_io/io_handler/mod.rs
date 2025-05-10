cd4fdc404db0d4fd98473a28e4b286f0ef2661d1
whazzabii7 <whazzabii7@gmail.com>
2025-05-10 16:54:11 +0200

    splitet loops to separate files, lesser nesting

--- src/io/console_io/io_handler/mod.rs
+++ src/io/console_io/io_handler/mod.rs
@@ -1,26 +1,31 @@
-use std::io::{self, Read, Write};
+use std::io::{self, Write};
 use crate::{
-    io::console_io::IoMode,
+    io::console_io::{
+        colors::*,
+        IoMode,
+    },
     tct_error::IoError,
 };
-    
+
 mod raw_mode;
 use raw_mode::raw_mode::*;
+
+mod command_mode;
+mod input_mode;
 
 mod input_buffer;
 use input_buffer::*;
 
+
 pub struct IoHandler {
     buffer: InputBuffer,
-    os: String,
     input: io::Stdin,
     out: io::Stdout,
 }
 
 impl IoHandler {
-    pub fn init(os: String) -> Self {
+    pub fn init() -> Self {
         Self {
-            os,
             buffer: InputBuffer::new(),
             out: io::stdout(),
             input: io::stdin(),
@@ -28,11 +33,11 @@ impl IoHandler {
     }
 
     pub fn print_banner(&self) -> Result<(), IoError> {
-        print!("\x1B[1;36m");
+        print!("{}", Colorize::Fg(Color::BannerColor));
         println!("╔════════════════════════════╗");
-        println!("║        TCT CLI Tool        ║");
+        println!("║   Terminal Call Tracker    ║");
         println!("╚════════════════════════════╝");
-        print!("\x1B[0m");
+        print!("{}", Colorize::Reset);
         self.flush()?;
         Ok(())
     }
@@ -61,32 +66,24 @@ impl IoHandler {
             Ok(c) => if c == 0 { return Err(IoError::FailedRead);},
             Err(e) => return Err(IoError::Io(e)),
         }
-
         Ok(())
     }
 
-    pub fn input_mode(&mut self, mode: &IoMode) -> std::io::Result<()> {
-        let _raw = RawMode::enable()?;
-        _ = self.prompt(mode);
-        for byte in self.input.lock().bytes() {
-            let b = byte?;
-            match b {
-                27 => {
-                    println!("\nESC pressed. BYE!");
-                    break;
-                }
-                b'\n' => {
-                    let out = self.buffer.flush().unwrap();
-                    println!("\nInput: {:?}", out);
-                    break;
-                }
-                _ => {
-                    print!("{}", char::from(b));
-                    _ = self.flush();
-                    self.buffer.push(b);
-                }
-            }
+    pub fn command_mode(&mut self) -> Result<(), IoError> {
+        print!("{}[TCT]{} Start interactive session...\n",
+            Colorize::Fg(Color::TctGreen), Colorize::Reset);
+        print!("{}[INFO]{} Enter 'q' to quit.\n\n",
+            Colorize::Fg(Color::InfoBlue), Colorize::Reset);
+        command_mode::command_loop(self)?;
+        Ok(())
+    }
+
+    pub fn input_mode(&mut self) -> Result<(), IoError> {
+        if let Err(e) = RawMode::enable() { 
+            return Err(IoError::Io(e))
         }
+        self.prompt(&IoMode::Input)?;
+        input_mode::input_loop(self)?;
         Ok(())
     }
 }
